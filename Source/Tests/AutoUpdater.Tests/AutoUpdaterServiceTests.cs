ae61eed631a1041a6ad48b969c010497fb78b5aa
Bobby Rachkov <bobi.rachkov@outlook.com>
2025-01-19 22:56:41 +0200

    CR cleanup

--- Source/Tests/AutoUpdater.Tests/AutoUpdaterServiceTests.cs
+++ Source/Tests/AutoUpdater.Tests/AutoUpdaterServiceTests.cs
@@ -13,13 +13,7 @@ namespace AutoUpdater.Tests
         [SetUp]
         public void Setup()
         {
-            //_handler = new MockHttpMessageHandler();
-            //_httpClient = new HttpClient(_handler);
 
-            //var httpClientFactoryMock = new Mock<IHttpClientFactory>();
-            //httpClientFactoryMock.Setup<HttpClient>(x => x.CreateClient(It.IsAny<string>())).Returns(_httpClient);
-
-            //_sut = new AutoUpdaterService(httpClientFactoryMock.Object);
         }
 
         [TearDown]
@@ -28,37 +22,6 @@ namespace AutoUpdater.Tests
             _httpClient.Dispose();
             _handler.Dispose();
         }
-
-        //[Theory]
-        //public async Task GivenCheckingForLatestVersion_WhenResponseIsIncorrectJson_ThenReturnsNull(ReleaseType releaseType)
-        //{
-        //    //Arrange
-        //    _handler.When("https://api.github.com/repos/BobbyRachkov/InspectaQueue/releases")
-        //        .Respond("application/json", "{'name' : 'Test McGee'}");
-
-        //    //Act
-        //    var release = await _sut.GetLatestVersion(releaseType);
-
-        //    //Assert
-        //    Assert.That(release, Is.Null);
-        //}
-
-        //[TestCase(ReleaseType.Official, "0.1.2")]
-        //[TestCase(ReleaseType.Prerelease, "0.1.1")]
-        //public async Task GivenCheckingForLatestVersion_WhenResponseIsCorrectJson_ThenReturnsExpectedVersion(
-        //    ReleaseType releaseType,
-        //    string expectedVersion)
-        //{
-        //    //Arrange
-        //    _handler.When("https://api.github.com/repos/BobbyRachkov/InspectaQueue/releases")
-        //        .Respond("application/json", TestData.ReleasesResponse);
-
-        //    //Act
-        //    var release = await _sut.GetLatestVersion(releaseType);
-
-        //    //Assert
-        //    Assert.That(release.Value.version.ToString(), Is.EqualTo(expectedVersion));
-        //}
 
         [Ignore("Used for development purposes")]
         [Test]
