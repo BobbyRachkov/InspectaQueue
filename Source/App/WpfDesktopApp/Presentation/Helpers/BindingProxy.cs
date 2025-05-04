ccc04c91e307d8f4384718515425fd87bb81f66e
Bobby Rachkov <bobi.rachkov@outlook.com>
2025-05-04 18:00:27 +0300

    fix everything

---
+++ Source/App/WpfDesktopApp/Presentation/Helpers/BindingProxy.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+
+namespace Rachkov.InspectaQueue.WpfDesktopApp.Presentation.Helpers
+{
+    public class BindingProxy : Freezable
+    {
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        public object Data
+        {
+            get => GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register(nameof(Data), typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+}
